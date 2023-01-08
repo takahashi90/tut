e476c67d369568e245d93c72a04ad2f5abb4eae8
Muhammad Haseeb <109789195+takahashi90@users.noreply.github.com>
2023-01-08 18:54:33 +0500

    sync commit

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from .import views
 
 urlpatterns = [
     path('',views.home, name="home"), # it is really convinient to name our urls
-    path('room/',views.room, name="room"),
+    path('room/<str:pk>/',views.room, name="room"),
     ]
--- base/views.py
+++ base/views.py
@@ -5,16 +5,20 @@ from django.shortcuts import render
 
 rooms = [
 
-    {id:'1', 'name': 'Lets learn python'},
-    {id:'2', 'name': 'Design with me'},
-    {id:'1', 'name': 'Frontend devs'},
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend devs'},
 ]
 
 def home(request):
     context = {'rooms': rooms}
-    return render(request, 'home.html',context) # Now we can use render # as we have added rooms list in home functions so we would only be able to add them to home.html not room 
-    # return HttpResponse('Home page')
+    return render(request, 'base/home.html',context) # Now we can use render # as we have added rooms list in home functions so we would only be able to add them to home.html not room 
     
-def room(request):    
-    return render(request, 'room.html')
-    # return HttpResponse('ROOM')
+    
+def room(request, pk):
+    room = None    
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i  
+    context = {'room': room}
+    return render(request, 'base/room.html', context)  # we will have to specify path for our room and home html files because they are inside our app 
