6ef66e9e614ee64a087088e50381e499b7c20307
Muhammad Haseeb <109789195+takahashi90@users.noreply.github.com>
2023-01-10 20:28:41 +0500

    sync commit

--- base/models.py
+++ base/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
     # host = 
-    # topic =
+    topic = models.ForeignKey(Topic, on_delete = models.SET_NULL, null=True)
     name = models.CharField(max_length=200) # we have to specify the type of the model
     description = models.TextField(null=True, blank=True) # null being true means that description can be empty it is to give user choice to add description later or to never add one and blank = true means that when form is submitted, it could also be empty
     # participants =
@@ -13,3 +22,13 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+class Message(models.Model): # child model of Room
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    room = models.ForeignKey(Room, on_delete = models.CASCADE)
+    body = models.TextField() # the message that user would write 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.body[0:50] # it would show only first 50 chracters so if the message is long preview would show only 50 chracter part of it 
